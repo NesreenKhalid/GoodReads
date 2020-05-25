435df4c5a202fa3ad0129e303b37b2b788077365
Ibrahim Kholy <ibrahem9311@yahoo.com>
2020-05-25 23:02:50 +0200

    update front end

--- my-app/src/components/catigories/catigoris.js
+++ my-app/src/components/catigories/catigoris.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 // import UserService from "../../services/user.service";
 import cat1 from '../../assets/cat1.jpg';
@@ -7,97 +7,109 @@ import cat3 from '../../assets/cat3.jpg';
 import cat4 from '../../assets/cat4.jpg';
 import cat5 from '../../assets/cat5.png';
 import cat6 from '../../assets/cat6.jpg';
-export default class Catigories extends Component {
 
-    render() {
-        return (
 
-            <div className="container">
-                <div className="row">
-                    <div className="card" >
-                        <img className="cat1 card-img-top" src={cat1} alt="econimics" />
-                        <div className="card-body">
-                            <h5 className="card-title">Econimics</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+export default function Catigories() {
+    const [state, setstate] = React.useState([]);
+    const [haserror, seterror] = React.useState(false);
+
+    React.useEffect(() => {
+        fetch('').then((respons)=>respons.json().then((resJson)=>{
+            setstate(resJson)
+        })).catch(err=>seterror(true))
+    }, [])
+
+    return (
+
+        <div className="container">
+
+            {/* {haserror ? <div> some thing wrong</div> :state.map(item=> */}
+                <div>
+                    <div className="row">
+                        <div className="card" >
+                            <img className="cat1 card-img-top" src={cat1} alt="econimics" />
+                            <div className="card-body">
+                                <h5 className="card-title">Econimics</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+
+                            <div className="card-body">
+                                <Link to="#" className="card-link">Card link</Link>
+
+                            </div>
                         </div>
-                        
-                        <div className="card-body">
-                            <Link to="#" className="card-link">Card link</Link>
-                            
+
+                        <div className="card" >
+                            <img className="cat1 card-img-top" src={cat2} alt="society" />
+                            <div className="card-body">
+                                <h5 className="card-title">Society</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+
+                            <div className="card-body">
+                                <Link to="#" className="card-link">Card link</Link>
+
+                            </div>
                         </div>
+
+                        <div className="card" >
+                            <img className="cat1 card-img-top" src={cat3} alt="econimics" />
+                            <div className="card-body">
+                                <h5 className="card-title">Horror</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+
+                            <div className="card-body">
+                                <Link to="#" className="card-link">Card link</Link>
+
+                            </div>
+                        </div>
+
                     </div>
-                    {/* cart1 */}
-                    <div className="card" >
-                        <img className="cat1 card-img-top" src={cat2} alt="society" />
-                        <div className="card-body">
-                            <h5 className="card-title">Society</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <div className="row">
+                        <div className="card" >
+                            <img className="cat1 card-img-top" src={cat4} alt="econimics" />
+                            <div className="card-body">
+                                <h5 className="card-title">Fantasty</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+
+                            <div className="card-body">
+                                <Link to="#" className="card-link">Card link</Link>
+
+                            </div>
                         </div>
-                        
-                        <div className="card-body">
-                            <Link to="#" className="card-link">Card link</Link>
-                            
+
+                        <div className="card" >
+                            <img className="cat1 card-img-top" src={cat5} alt="econimics" />
+                            <div className="card-body">
+                                <h5 className="card-title">Romance</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+
+                            <div className="card-body">
+                                <Link to="#" className="card-link">Card link</Link>
+
+                            </div>
                         </div>
+
+                        <div className="card" >
+                            <img className="cat1 card-img-top" src={cat6} alt="econimics" />
+                            <div className="card-body">
+                                <h5 className="card-title">Fiction</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+
+                            <div className="card-body">
+                                <Link to="#" className="card-link">Card link</Link>
+
+                            </div>
+                        </div>
+
                     </div>
-                    {/* cart2 */}
-                    <div className="card" >
-                        <img className="cat1 card-img-top" src={cat3} alt="econimics" />
-                        <div className="card-body">
-                            <h5 className="card-title">Horror</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                        
-                        <div className="card-body">
-                            <Link to="#" className="card-link">Card link</Link>
-                            
-                        </div>
-                    </div>
-                    {/* cart3 */}
                 </div>
-                {/* new row */}
+                {/* )} */}
+        </div>
+    );
 
-                <div className="row">
-                    <div className="card" >
-                        <img className="cat1 card-img-top" src={cat4} alt="econimics" />
-                        <div className="card-body">
-                            <h5 className="card-title">Fantasty</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                        
-                        <div className="card-body">
-                            <Link to="#" className="card-link">Card link</Link>
-                            
-                        </div>
-                    </div>
-                    {/* cart1 */}
-                    <div className="card" >
-                        <img className="cat1 card-img-top" src={cat5} alt="econimics" />
-                        <div className="card-body">
-                            <h5 className="card-title">Romance</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                        
-                        <div className="card-body">
-                            <Link to="#" className="card-link">Card link</Link>
-                            
-                        </div>
-                    </div>
-                    {/* cart2 */}
-                    <div className="card" >
-                        <img className="cat1 card-img-top" src={cat6} alt="econimics" />
-                        <div className="card-body">
-                            <h5 className="card-title">Fiction</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                        
-                        <div className="card-body">
-                            <Link to="#" className="card-link">Card link</Link>
-                            
-                        </div>
-                    </div>
-                    {/* cart3 */}
-                </div>
-            </div>
-        );
-    }
 }

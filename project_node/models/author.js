7f85226b96e84bb204de3fb01cfa75b3421edebb
NesreenKhalid <d.nesreenkhalid@gmail.com>
2020-05-27 18:00:43 +0200

    merge

--- project_node/models/author.js
+++ project_node/models/author.js
@@ -4,14 +4,10 @@ const AuthorSchema = new mongoose.Schema({
     authorImage:{type:String},// required:true},
     firstName:{type:String,required:true,minlength:3,maxlength:15},
     lastName:{type:String,required:true,minlength:3,maxlength:15},
-<<<<<<< HEAD
-    dateOfBirthdateOfBirth:{type:Date }//, required:true },
-=======
     dateOfBirth:{type:Date
         ,
         required:true
     },
->>>>>>> 8bc3d4df015e035d2e3fcffddb0dbe895a1deb8b
 })
 const AuthorModel = mongoose.model('author',AuthorSchema)
 module.exports = AuthorModel;

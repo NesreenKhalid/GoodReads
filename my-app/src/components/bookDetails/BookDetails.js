7b4b135532947c209bd9ccebe9f174d2e5b9b89b
Ibrahim Kholy <ibrahem9311@yahoo.com>
2020-05-28 17:12:12 +0200

    solve conflict at book component

--- my-app/src/components/bookDetails/BookDetails.js
+++ my-app/src/components/bookDetails/BookDetails.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { Component } from 'react';
 import { DropdownButton, Dropdown } from 'react-bootstrap'
 import axios from 'axios'
 import Rating from 'material-ui-rating'
@@ -6,102 +6,159 @@ import AuthService from '../../services/auth.service'
 
 
 
-const BookDetails = (params) => {
-    const bookId = params.location.pathname.split('/')[2];
-    const userId = AuthService.getCurrentUser();
-    const finalUserId = userId.id;
-    const [book, setbook] = useState({});
-    const [reviews, setReviews] = useState([]);
-    const [totaluserShelvesandReviews, setUserShelvesandReviews] = useState({});
-    const [userRating, setUserRating] = useState(0);
-    const [userShelve, setUserShelve] = useState('');
-    const [userReview, setUserReview] = useState('');
-    const [submitted, setSubmitted] = useState(false)
-    useEffect(() => {
-        axios.get(`http://localhost:8000/book/${bookId}`)
+export default class UserTable extends Component {
+    state = {
+        book: {},
+        reviews: [],
+        totaluserShelvesandReviews: {},
+        userRating: 0,
+        userShelve: '',
+        userReview: '',
+        submitted: false
+    }
+    componentDidMount() {
+        const { match: { params } } = this.props;
+        console.log(params);
+        axios.get(`http://localhost:8000/book/${params.id}`)
             .then((response) => {
-                setbook(response.data);
-                setReviews(response.data.userShelvesandReveiews);
+                console.log(response)
+                this.setState({
+                    book: response.data,
+                    reviews: response.data.userShelvesandReveiews
+                });
             })
             .catch((err) => {
                 console.log(err);
             });
-    },[]);
-
-    function sendRelevantData() {
-        if (userReview === '') { setUserReview(totaluserShelvesandReviews.review) };
-        if (userRating === 0) { setUserRating(totaluserShelvesandReviews.rating) };
-        if (userShelve === '') { setUserShelve(totaluserShelvesandReviews.shelve) }
     }
-    useEffect(() => {
-        sendRelevantData();
-        axios.patch(`http://localhost:8000/book/userShelvesandReviews/${bookId}/${userId.id}`,
-            { "review": userReview, "rating": userRating, "shelve": userShelve })
+
+    componentDidUpdate() {
+    }
+
+    getBook(item) {
+        this.setState({ UserShelvesandReviews: item });
+    }
+   /* setUserShelves(shelve) {
+        this.setState({ userShelve: shelve })
+        const userId = AuthService.getCurrentUser();
+        const { match: { params } } = this.props;
+        console.log(userId.id,params.id);
+        if (this.state.userReview === '') { this.setState({ userReview: this.state.totaluserShelvesandReviews.review }) };
+        if (this.state.userRating === 0) { this.setState({ userRating: this.state.totaluserShelvesandReviews.rating }) };
+        if (this.state.userShelve === '') { this.setState({ userShelve: this.state.totaluserShelvesandReviews.shelve }) };
+        axios.patch(`http://localhost:8000/book/userShelvesandReviews/${params.id}/${userId.id}`,
+            { "shelve": this.state.userShelve })
             .then((success) => {
                 console.log(success);
             })
             .catch((failure) => {
                 console.log(failure);
             })
-    }, [userRating, userShelve, submitted])
+    }
+    setUserRating(rate) {
+        this.setState({ userRating: rate })
+        const userId = AuthService.getCurrentUser();
+        const { match: { params } } = this.props;
+        if (this.state.userReview === '') { this.setState({ userReview: this.state.totaluserShelvesandReviews.review }) };
+        if (this.state.userRating === 0) { this.setState({ userRating: this.state.totaluserShelvesandReviews.rating }) };
+        if (this.state.userShelve === '') { this.setState({ userShelve: this.state.totaluserShelvesandReviews.shelve }) };
+        axios.patch(`http://localhost:8000/book/userShelvesandReviews/${params.id}/${userId.id}`,
+            { "rating": this.state.userRating })
+            .then((success) => {
+                console.log(success);
+            })
+            .catch((failure) => {
+                console.log(failure);
+            })
+    }
+    setUserReview(value) {
+        this.setState({ userReview: value })
+    }
+    setFormSubmitted() {
+        this.setState({ submitted: !this.state.submitted })
+        const userId = AuthService.getCurrentUser();
+        const { match: { params } } = this.props;
+        if (this.state.userReview === '') { this.setState({ userReview: this.state.totaluserShelvesandReviews.review }) };
+        if (this.state.userRating === 0) { this.setState({ userRating: this.state.totaluserShelvesandReviews.rating }) };
+        if (this.state.userShelve === '') { this.setState({ userShelve: this.state.totaluserShelvesandReviews.shelve }) };
+        axios.patch(`http://localhost:8000/book/userShelvesandReviews/${params.id}/${userId.id}`,
+            { "review": this.state.userReview })
+            .then((success) => {
+                console.log(success);
+            })
+            .catch((failure) => {
+                console.log(failure);
+            })
+    }*/
 
 
-
-    const totalReviews = reviews.map((item) => {
-        if (item.userId == finalUserId) { setUserShelvesandReviews(item) }
+    render() {
+        const totalReviews = this.state.reviews.map((item) => {
+            const userId = AuthService.getCurrentUser();
+            if (item.userId === userId.id) { this.getBook(item) }
+            return (
+                <div className="card-body" key={item._id}>
+                    <h5 className="card-title">{item.userId}</h5>
+                    <p className="card-text">{item.review}</p>
+                </div>
+            )
+        })
         return (
-            <div className="card-body" key={item._id}>
-                <h5 className="card-title">{item.userId}</h5>
-                <p className="card-text">{item.review}</p>
+
+            <div className="container">
+                <div className="row">
+                    <div className="bookcard" >
+                        <div><img className="cat1 card-img-top" src={`http://localhost:8000/${this.state.book.image}`} alt="econimics" /></div>
+                        <div className="dropdown">
+                            <DropdownButton id="dropdown-item-button" title={this.state.totaluserShelvesandReviews.shelve}>
+                                <Dropdown.Item as="button" 
+                                //onClick={(event) => this.setUserShelves('Want to read')}
+                                >Want to read</Dropdown.Item>
+                                <Dropdown.Item as="button" 
+                                //onClick={(event) => this.setUserShelves('Reading')}
+                                >Reading</Dropdown.Item>
+                                <Dropdown.Item as="button" 
+                                //onClick={(event) => this.setUserShelves('Read')}
+                                >read</Dropdown.Item>
+                            </DropdownButton>
+                        </div>
+
+                        <div>
+                            <Rating value={this.state.userRating} max={5} 
+                            //onChange={(value) => this.setUserRating(value)} 
+
+                            />
+                        </div>
+                    </div>
+                    <div className="bookinfo">
+                        <h2>{this.state.book.name}</h2>
+                        <h3>{this.state.bookauthId}</h3>
+                        <h3>{this.state.book.catId}</h3>
+                        <div>
+                            <Rating value={this.state.book.avgRating} max={5} readOnly={true} />
+                            <p>{this.state.book.totalRatings}</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="book-reviews">
+                    <div className="cardreview">
+                        <div className="card-header">
+                            Reviews
+                    </div>
+                        {totalReviews}
+                    </div>
+                    <form 
+                    //onSubmit={() => { this.setFormSubmitted() }}
+                    >
+                        <input type="text" 
+                        //onChange={(value) => { this.setUserReview(value); console.log(this.state.userReview) }}
+
+                        ></input>
+                        <input type="submit" value="Add Comment"></input>
+                    </form>
+                </div>
             </div>
         )
-    })
-
-
-    
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="bookcard" >
-                    <div><img className="cat1 card-img-top" src={`http://localhost:8000/${book.image}`} alt="econimics" /></div>
-                    <div className="dropdown">
-                        <DropdownButton id="dropdown-item-button" title={userShelve}>
-                            <Dropdown.Item as="button" onClick={(event) => setUserShelve('Wants to read')}>Want to read</Dropdown.Item>
-                            <Dropdown.Item as="button" onClick={(event) => setUserShelve('Reading')}>Reading</Dropdown.Item>
-                            <Dropdown.Item as="button" onClick={(event) => setUserShelve('Read')}>read</Dropdown.Item>
-                        </DropdownButton>
-                    </div>
-
-                    <div>
-                        <Rating value={userRating} max={5} onChange={(value) => setUserRating(value)} />
-                    </div>
-                </div>
-                <div className="bookinfo">
-                    <h2>{book.name}</h2>
-                    <h3>{book.authId}</h3>
-                    <h3>{book.catId}</h3>
-                    <div>
-                        <Rating value={book.avgRating} max={5} readOnly={true} />
-                        <p>{book.totalRatings}</p>
-                    </div>
-                </div>
-            </div>
-            <div className="book-reviews">
-                <div className="cardreview">
-                    <div className="card-header">
-                        Reviews
-                    </div>
-                    {totalReviews}
-                </div>
-                <form>
-                    <input type="text" onChange={(value) => { setUserReview(value); console.log(userReview) }}></input>
-                    <input type="submit" value="Add Comment"></input>
-                </form>
-            </div>
-        </div>
-    )
-
+    }
 
 }
-
-export default BookDetails
